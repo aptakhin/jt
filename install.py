3c14e2bbecfe2bfb7b06c3ba9c2f352eb6db305a
Alexander Ptakhin <me@aptakhin.name>
2015-01-27 22:32:31 +0300

    Added python installation script currently for win32 binaries

---
+++ install.py
@@ -0,0 +1,48 @@
+import urllib2
+import sys
+from zipfile import ZipFile, BadZipfile
+
+def install():
+	print 'downloading win32 tools'
+	download_file('https://github.com/aptakhin/jt/releases/download/v0.0.0/wintools.zip', 'wintools.zip')
+
+	try:
+		with ZipFile('wintools.zip', 'r') as tools_zip:
+			test = tools_zip.testzip()
+			if test:
+				print 'archive corrupted, exit'
+				exit()
+
+			print 'extracting'
+			tools_zip.extractall('./')
+	except BadZipfile as e:
+		print 'can\'t open zip file: corrupted or something else, exit'
+		exit()
+
+def download_file(url, filename=None):
+	if not filename:
+		filename = url.split('/')[-1]
+
+	u = urllib2.urlopen(url)
+	f = open(filename, 'wb')
+	meta = u.info()
+	file_size = int(meta.getheaders('Content-Length')[0])
+	#print 'Downloading: %s Bytes: %s' % (filename, file_size)
+
+	file_size_dl = 0
+	block_sz = 8192
+	while True:
+	    buffer = u.read(block_sz)
+	    if not buffer:
+	        break
+
+	    file_size_dl += len(buffer)
+	    f.write(buffer)
+	    status = r'%10d  [%3.2f%%]' % (file_size_dl, file_size_dl * 100. / file_size)
+	    status = status + chr(8) * (len(status) + 1)
+	    print status,
+
+	f.close()
+	print
+
+install()
